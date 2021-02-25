0aecead909bbc56ecb659f33c81e08d8dfd9c378
Lijiangwei <604799353@qq.com>
2021-02-25 14:35:49 +0800

    data和组件传值

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- 在vue中使用for循环必须制定一个唯一的的值 ID或者是下标 -->
+    <div :key="todo.index" v-for="(todo, index) in todos">
+      {{ todo.title }} - {{ index }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todos", //组件名称
+  //接受数据的两种方式
+  //   props: ["todos"],
+  props: {
+    todos: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
